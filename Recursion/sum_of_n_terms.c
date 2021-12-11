8104f5fb23189443af41f301b1db5aff330631e6
Dark-Prince237 <rashidiqbal7141@gmail.com>
2021-12-11 17:03:13 +0530

    Q1 sum of nth number done

---
+++ Recursion/sum_of_n_terms.c
@@ -0,0 +1,21 @@
+// sum of nth number of term in recursion
+#include<stdio.h>
+int sum_of_nth(int);
+int main()
+{
+    int n,sum;
+    printf("enter nth term: ");
+    scanf("%d",&n);
+    sum=sum_of_nth(n);
+    printf("%d",sum);
+    
+}
+
+int sum_of_nth(int n)
+{
+    if(n==1)
+        return 1;
+
+    else
+        return n+sum_of_nth(n-1);
+}
